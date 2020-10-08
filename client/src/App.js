6dae909401065743316bfdb9483550d8d949f56d
Josh Lane <joshdlane22@gmail.com>
2020-10-08 17:11:37 -0400

    adding title

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import FileUpload from './components/fileUpload';
 import Analysis from './components/mainAnalysis'
-
+import Title from './components/title'
 import './App.css';
 
 function App() {
@@ -14,6 +14,7 @@ function App() {
 
   return (
       <div className="app-container">
+        <Title/>
         <FileUpload onUploadChange={(status) => handleUploadStatus(status)}/>
         <Analysis isData={dataUploaded}/>
       </div>
---
+++ client/src/components/title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./title.scss"
+
+const Title = () => {
+    return (
+        <div className="main-title">
+            <span>FASTQ Quality Control</span>
+        </div>
+    );
+};
+
+export default Title;
